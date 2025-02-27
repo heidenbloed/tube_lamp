042d59fb288847e7cb756dfbe725b374361bbc12
heidenbloed <69988488+heidenbloed@users.noreply.github.com>
2025-02-27 20:12:53 +0100

    Add rust formatting rules and apply them.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
-    // It is necessary to call this function once. Otherwise some patches to the runtime
-    // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
+    // It is necessary to call this function once. Otherwise some patches to the
+    // runtime implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
     esp_idf_svc::sys::link_patches();
 
     // Bind the log crate to the ESP Logging facilities
