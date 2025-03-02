c24171668f35fdb7413a8847c4d5d8c03571bb98
heidenbloed <69988488+heidenbloed@users.noreply.github.com>
2025-03-02 15:02:47 +0100

    Subscribe to all topics and handle receiving messages.

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,15 @@ const MQTT_URL: &str = dotenv!("MQTT_URL");
 const MQTT_USERNAME: &str = dotenv!("MQTT_USERNAME");
 const MQTT_PASSWORD: &str = dotenv!("MQTT_PASSWORD");
 const MQTT_CLIENT_ID: &str = dotenv!("MQTT_CLIENT_ID");
-const MQTT_TOPIC: &str = dotenv!("MQTT_TOPIC");
+
+const MQTT_TOPIC_MODE: &str = "lamps/tube/mode";
+const MQTT_TOPIC_RGB: &str = "lamps/tube/rgb";
+const MQTT_TOPIC_HSV: &str = "lamps/tube/hsv";
+const MQTT_TOPIC_HEX: &str = "lamps/tube/hex";
+const MQTT_TOPIC_WARM: &str = "lamps/tube/warm";
+const MQTT_TOPIC_PROGRESS: &str = "lamps/tube/progress";
+const MQTT_TOPIC_WHEEL_SPEED: &str = "lamps/tube/wheel_speed";
+const MQTT_TOPIC_LOG: &str = "lamps/tube/log";
 
 fn main() {
     esp_idf_svc::sys::link_patches();
@@ -38,16 +46,15 @@ fn main() {
         let (mut client, mut conn) = connect_mqtt().expect("Failed to connect to MQTT broker.");
 
         let mut timer = timer_service.timer_async()?;
-        run(&mut client, &mut conn, &mut timer, MQTT_TOPIC).await
+        run_mqtt(&mut client, &mut conn, &mut timer).await
     })
     .unwrap();
 }
 
-async fn run(
+async fn run_mqtt(
     client: &mut EspAsyncMqttClient,
     connection: &mut EspAsyncMqttConnection,
     timer: &mut EspAsyncTimer,
-    topic: &str,
 ) -> Result<(), EspError> {
     info!("About to start the MQTT connection.");
 
@@ -55,25 +62,38 @@ async fn run(
         pin!(async move {
             info!("MQTT Listening for messages.");
             while let Ok(event) = connection.next().await {
-                info!("[Queue] Event: {}", event.payload());
+                handle_mqtt_event(&event.payload());
             }
             info!("Connection closed");
             Ok(())
         }),
         pin!(async move {
-            loop {
-                if let Err(e) = client.subscribe(topic, QoS::AtMostOnce).await {
-                    error!("Failed to subscribe to topic \"{topic}\": {e}, retrying...");
-                    timer.after(Duration::from_millis(500)).await?;
-                    continue;
-                }
-                info!("Subscribed to topic \"{topic}\"");
-                timer.after(Duration::from_millis(500)).await?;
+            for topic in [
+                MQTT_TOPIC_MODE,
+                MQTT_TOPIC_RGB,
+                MQTT_TOPIC_HSV,
+                MQTT_TOPIC_HEX,
+                MQTT_TOPIC_WARM,
+                MQTT_TOPIC_PROGRESS,
+                MQTT_TOPIC_WHEEL_SPEED,
+            ]
+            .iter()
+            {
                 loop {
-                    let sleep_secs = 2;
-                    info!("Now sleeping for {sleep_secs}s...");
-                    timer.after(Duration::from_secs(sleep_secs)).await?;
+                    if let Err(e) = client.subscribe(topic, QoS::AtMostOnce).await {
+                        error!("Failed to subscribe to topic \"{topic}\": {e}, retrying...");
+                        timer.after(Duration::from_millis(500)).await?;
+                        continue;
+                    }
+                    info!("Subscribed to topic \"{topic}\"");
+                    break;
                 }
+            }
+
+            timer.after(Duration::from_millis(500)).await?;
+            loop {
+                tube_lamp_tick()?;
+                timer.after(Duration::from_millis(10)).await?;
             }
         }),
     )
@@ -83,6 +103,68 @@ async fn run(
         Either::First(res) => res,
         Either::Second(res) => res,
     }
+}
+
+fn handle_mqtt_event(event_payload: &EventPayload<'_, EspError>) {
+    match event_payload {
+        EventPayload::Received {
+            id,
+            topic,
+            data,
+            details: _,
+        } => {
+            if let Ok(msg) = core::str::from_utf8(data) {
+                info!("Received MQTT message from id \"{id}\" on topic \"{topic:?}\": {msg:?}.");
+                match topic {
+                    Some(MQTT_TOPIC_MODE) => {
+                        info!("Received mode change message.");
+                        warn!("Not yet implemented.");
+                    }
+                    Some(MQTT_TOPIC_RGB) => {
+                        info!("Received color change (RGB) message.");
+                        warn!("Not yet implemented.");
+                    }
+                    Some(MQTT_TOPIC_HSV) => {
+                        info!("Received color change (HSV) message.");
+                        warn!("Not yet implemented.");
+                    }
+                    Some(MQTT_TOPIC_HEX) => {
+                        info!("Received color change (HEX) message.");
+                        warn!("Not yet implemented.");
+                    }
+                    Some(MQTT_TOPIC_WARM) => {
+                        info!("Received color change (warm) message.");
+                        warn!("Not yet implemented.");
+                    }
+                    Some(MQTT_TOPIC_PROGRESS) => {
+                        info!("Received progress change message.");
+                        warn!("Not yet implemented.");
+                    }
+                    Some(MQTT_TOPIC_WHEEL_SPEED) => {
+                        info!("Received wheel change message.");
+                        warn!("Not yet implemented.");
+                    }
+                    Some(topic) => {
+                        error!("Unexpected MQTT topic: \"{topic}\".");
+                    }
+                    None => {
+                        error!("No MQTT topic.");
+                    }
+                }
+            } else {
+                error!("Failed to parse MQTT message from id \"{id}\" on topic \"{topic:?}\".");
+            }
+        }
+        _ => {
+            info!("Other MQTT event occurred: {event_payload}");
+        }
+    }
+}
+
+fn tube_lamp_tick() -> Result<(), EspError> {
+    debug!("Tube lamp tick");
+    warn!("Not yet implemented.");
+    Ok(())
 }
 
 fn connect_mqtt() -> Result<(EspAsyncMqttClient, EspAsyncMqttConnection), EspError> {
